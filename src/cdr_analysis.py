587653bca24e15350aa8ef3c83e09b40546520b7
zubinengg <zubin.engg@gmail.com>
2015-03-23 10:25:57 +0530

    BSNL

--- src/cdr_analysis.py
+++ src/cdr_analysis.py
@@ -8,27 +8,51 @@ __date__ = "$Mar 20, 2015 2:29:40 PM$"
 def idea():
     file_r = 'F:\\Ravi Work\\Raid Case\\idea\\idea.csv'
     read_f = open(file_r, 'r')
-    out_w=open('F:\\Ravi Work\\Raid Case\\idea\\out.txt','w')
-    line=read_f.readline()
-    count=0
-    out_w.write('IDEA_NO|'+line)
-    line=read_f.readline()
-    while line :
-        count+=1
-        data=line.split('|')
+    out_w = open('F:\\Ravi Work\\Raid Case\\idea\\out.txt', 'w')
+    line = read_f.readline()
+    count = 0
+    out_w.write('IDEA_NO|' + line)
+    line = read_f.readline()
+    while line:
+        count += 1
+        data = line.split('|')
         #print (line.strip('\n'))
-        if len(data)>8 :
-            if data[6]=='MO':
-                temp=data[0]
-                out_w.write(temp+'|'+line)
-            if data[6]=='MT':
-                temp=data[1]
-                out_w.write(temp+'|'+line)                        
-        line=read_f.readline()
+        if len(data) > 8:
+            if data[6] == 'MO':
+                temp = data[0]
+                out_w.write(temp + '|' + line)
+            if data[6] == 'MT':
+                temp = data[1]
+                out_w.write(temp + '|' + line)                        
+        line = read_f.readline()
         
-        if count%100000==0 :
+        if count % 100000 == 0:
             print (count)        
 
+def BSNL():
+    file_r = 'F:\\Ravi Work\\Raid Case\\BSNL\\BSNL.txt'
+    read_f = open(file_r, 'r')
+    out_w = open('F:\\Ravi Work\\Raid Case\\BSNL\\out.txt', 'w')
+    line = read_f.readline()
+    count = 0
+    out_w.write('BSNL_NO|' + line)
+    line = read_f.readline()
+    while line:
+        count += 1
+        data = line.split('|')
+        #print (line.strip('\n'))
+        if len(data) > 8:
+            if data[7] == 'MOC':
+                temp = data[0]
+                out_w.write(temp + '|' + line)
+            if data[7] == 'MTC':
+                temp = data[1]
+                out_w.write(temp + '|' + line)                        
+        line = read_f.readline()
+        
+        if count % 100000 == 0:
+            print (count)       
+            
 def see_sample(file_name):
     reader = open(file_name, 'r')
     line = reader.readline()
@@ -45,4 +69,5 @@ def see_sample(file_name):
             
 if __name__ == "__main__":
     print ("Hello World")
-    
+    #see_sample('F:\\Ravi Work\\Raid Case\\BSNL\\BSNL.txt')
+    BSNL()
